dfca5e744065f138b9f4e137038c8d5b58baaf3c
Prabesh <prabesh.bista369@gmail.com>
2021-12-26 15:23:06 +0545

    change

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-console.log("My name is prabesh bista");
-let name1="Prabesh Bista";
-//cannot be a reserved key
-// should be meaningful
-//cannot start with a number
-//cannot contain a space or hyphen(-)
-let firstName="Prabesh";
-let lastName="Bista";
-console.log(name1);
-console.log(firstName);
-console.log(lastName);
+// console.log("My name is prabesh bista");
+// let name1="Prabesh Bista";
+// //cannot be a reserved key
+// // should be meaningful
+// //cannot start with a number
+// //cannot contain a space or hyphen(-)
+// let firstName="Prabesh";
+// let lastName="Bista";
+// console.log(name1);
+// console.log(firstName);
+// console.log(lastName);
+
+const myHeading=document.querySelector('h1');
+myHeading.textContent='Hello World!';
