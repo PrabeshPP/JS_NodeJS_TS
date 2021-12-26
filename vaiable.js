67192d007328bed8c960104dbb0ef597a34f8b32
Prabesh <prabesh.bista369@gmail.com>
2021-12-26 14:37:06 +0545

    variables

--- vaiable.js
+++ vaiable.js
@@ -1,4 +1,26 @@
  const interestRate=0.3;
  console.log(interestRate);
  
+/*
+Primitive Type
+-String
+-Number
+-Boolean
+-undefined
+-null
+*/
+
+let name="Prabesh"; //String literal
+let number=300; //Number literal
+let istrue=false; //Boolean Literal
+let firstName; //undefined literal
+let lastName=null; //null literal
+
+/*
+reference
+Object,
+Arrays,
+Functions
+*/
+
 
