ac3fab7a3b235cd698af34bfbb64785d95858339
Prabesh <prabesh.bista369@gmail.com>
2021-12-26 14:48:26 +0545

    array

--- array.js
+++ array.js
@@ -1,3 +1,10 @@
+//An Array is dynamic
+// An array is an object
+
 
 let selectionColors=['Red','Blue'];
 console.log(selectionColors[1]);
+selectionColors[2]='gray';
+console.log(selectionColors);
+selectionColors[3]=100;
+console.log(selectionColors);
