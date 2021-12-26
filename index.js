0c8e77147644de6c2760883007e7a30ef6f103a9
Prabesh <prabesh.bista369@gmail.com>
2021-12-26 14:24:10 +0545

    .

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("My name is prabesh bista");
+let name1="Prabesh Bista";
+//cannot be a reserved key
+// should be meaningful
+//cannot start with a number
+//cannot contain a space or hyphen(-)
+let firstName="Prabesh";
+let lastName="Bista";
+console.log(name1);
+console.log(firstName);
+console.log(lastName);
