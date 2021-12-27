56ea61ff9b05bb3beaac1132101ef3833610f810
Prabesh <prabesh.bista369@gmail.com>
2021-12-27 17:00:11 +0545

    for,do,do-while loo

--- nodejs/node12.js
+++ nodejs/node12.js
@@ -1,7 +1,27 @@
 function random(){
-    let x=0;
-    do {
+   let x=0;
+
+// while loop
+    while(x<10){
         console.log(x);
-        x++;
-    } while (x<10);
+        x+=1;
+    }
+
+// for loop
+x=10;
+for(i=0;i<=x;i++){
+    if(i%2==0){
+    console.log("Even number"+i);
+    }
 }
+// do loop
+ x=0;
+do {
+    console.log(x);
+    x++;
+} while (x<10);
+}
+
+
+
+random();
