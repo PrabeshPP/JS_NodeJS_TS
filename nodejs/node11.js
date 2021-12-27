b80cb2991d21319bffb37dd5cf220c32aaeb8201
Prabesh <prabesh.bista369@gmail.com>
2021-12-27 16:40:45 +0545

    comment,start node js

--- nodejs/node11.js
+++ nodejs/node11.js
@@ -1,2 +1,8 @@
-// node basic
+// node Js Prerequisites
+// basic JavaScript
+// Advanced Javascript will be bonus
+// ECMAScript 6 --ES5 & ES6
+// Client Server Model (optional)
+
+
 console.log("Hello node js");
