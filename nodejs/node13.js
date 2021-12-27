bbff780d95f60d395724abff10149141536e352f
Prabesh <prabesh.bista369@gmail.com>
2021-12-27 18:39:00 +0545

    append file

--- nodejs/node13.js
+++ nodejs/node13.js
@@ -2,5 +2,5 @@ const fs=require("fs");
 
 const name1="bista";
 console.log(name1);
-fs.writeFileSync('read.txt','welcome to the metaverse');
+fs.appendFileSync('read.txt','welcome to the metaverse');
 fs.appendFileSync('read.txt','\nBlockChian is the future');
