9afa91e003e2efcc9fa96a62ada66e81020884a6
Prabesh <prabesh.bista369@gmail.com>
2021-12-27 18:48:27 +0545

    fread,write,rename

--- nodejs/node13.js
+++ nodejs/node13.js
@@ -2,5 +2,12 @@ const fs=require("fs");
 
 const name1="bista";
 console.log(name1);
-fs.appendFileSync('read.txt','welcome to the metaverse');
-fs.appendFileSync('read.txt','\nBlockChian is the future');
+// fs.appendFileSync('read.txt','welcome to the metaverse');
+// fs.appendFileSync('read.txt','\nBlockChian is the future');
+const buff_data=fs.readFileSync('read.txt');
+
+let org_data=buff_data.toString();
+console.log(org_data);
+
+//to rename the file
+fs.renameSync('read.txt','prabesh.text');
