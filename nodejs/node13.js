9a091a6d38a84e710a02c617f35b1432456118a4
Prabesh <prabesh.bista369@gmail.com>
2021-12-27 18:37:51 +0545

    file system,module

--- nodejs/node13.js
+++ nodejs/node13.js
@@ -3,4 +3,4 @@ const fs=require("fs");
 const name1="bista";
 console.log(name1);
 fs.writeFileSync('read.txt','welcome to the metaverse');
-fs.writeFileSync('read.txt','BlockChian is the future');
+fs.appendFileSync('read.txt','\nBlockChian is the future');
