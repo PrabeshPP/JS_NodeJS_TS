e12d48285e047f600dea00c288f9ed50b79fbe6b
Prabesh <prabesh.bista369@gmail.com>
2021-12-28 16:01:17 +0545

    path module

--- nodejs/path.js
+++ nodejs/path.js
@@ -1,2 +1,10 @@
 const path=require('path');
-console.log(path.extname("C:/Users/prabe/OneDrive/Desktop/JavaScript/nodejs/path.js"));
+// it returns the a line with dir path
+console.log(path.dirname("C:/Users/prabe/OneDrive/Desktop/JavaScript/nodejs/path.js"));
+// it returns type of file,in this case it returs js(java script)
+console.log(path.extname('C:/Users/prabe/OneDrive/Desktop/JavaScript/nodejs/path.js'));
+// it returns the file name
+console.log(path.basename('C:/Users/prabe/OneDrive/Desktop/JavaScript/nodejs/path.js'));
+// it parse the path into several pieces
+const mypath=path.parse('C:/Users/prabe/OneDrive/Desktop/JavaScript/nodejs/path.js');
+console.log(mypath.base);
