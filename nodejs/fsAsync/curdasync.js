acd3221c10d610118f02ef65655fd1a80f8bc0b0
Prabesh <prabesh.bista369@gmail.com>
2021-12-28 14:50:24 +0545

    curd async file

--- nodejs/fsAsync/curdasync.js
+++ nodejs/fsAsync/curdasync.js
@@ -1,6 +1,6 @@
 const fs=require('fs');
 
-//creating and writing in an async file
+// creating and writing in an async file
 fs.writeFile('read.text','this is an async file!',(err)=>{
 console.log("files is created");
 });
@@ -11,7 +11,10 @@ fs.appendFile('read.text',"\nThis is a second line",(err)=>
     console.log("file appended!");
 });
 
-//reading the data from an async file
+
+
+// reading the data from an async file
 fs.readFile('read.text','utf8',(err,data)=>{
     console.log(data);
 });
+console.log("Hellow world")
