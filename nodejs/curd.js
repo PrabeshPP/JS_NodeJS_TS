6bb4c80fea3248796737d4902858f45b0fcf732c
Prabesh <prabesh.bista369@gmail.com>
2021-12-28 14:05:10 +0545

    append

--- nodejs/curd.js
+++ nodejs/curd.js
@@ -1,5 +1,6 @@
 const fs=require('fs');
 
-fs.writeFileSync('op.txt','My name is prabesh bista,Have a good time!')
+// fs.writeFileSync('op.txt','My name is prabesh bista,Have a good time!')
+fs.appendFileSync("op.txt","\nThis is second line.")
 readline=fs.readFileSync('op.txt').toString();
 console.log(readline);
