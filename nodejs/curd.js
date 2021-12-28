edee1c5a89e4aaeb313985c626d846295d547d1a
Prabesh <prabesh.bista369@gmail.com>
2021-12-28 14:07:24 +0545

    comment

--- nodejs/curd.js
+++ nodejs/curd.js
@@ -1,6 +1,6 @@
 const fs=require('fs');
 
-// fs.writeFileSync('op.txt','My name is prabesh bista,Have a good time!')
-fs.appendFileSync("op.txt","\nThis is second line.")
-readline=fs.readFileSync('op.txt').toString();
+fs.writeFileSync('op.txt','My name is prabesh bista,Have a good time!') // this line will write the file and if call again it will override the file
+fs.appendFileSync("op.txt","\nThis is second line.") //this line will add the data to an existing file
+readline=fs.readFileSync('op.txt').toString(); //this line will read the file and will return the data of the file
 console.log(readline);
