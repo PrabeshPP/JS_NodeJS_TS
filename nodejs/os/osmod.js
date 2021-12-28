2440fcff4b48b17f153e6a59174aa19b11858eab
Prabesh <prabesh.bista369@gmail.com>
2021-12-28 15:13:20 +0545

    operating system

---
+++ nodejs/os/osmod.js
@@ -0,0 +1,11 @@
+//exploring with operating system command
+const os=require('os');
+console.log(os.arch());
+
+const freeMemory=os.freemem();
+const inGIBI=freeMemory/1024/1024/1024;
+console.log(inGIBI);
+
+console.log(os.totalmem()/1024/1024/1024);
+console.log(os.hostname());
+console.log(os.type());
