7a46898821bc131589ba201c225aa96e9eb45646
Prabesh <prabesh.bista369@gmail.com>
2021-12-28 14:02:07 +0545

    curd

--- nodejs/curd.js
+++ nodejs/curd.js
@@ -1,5 +1,5 @@
 const fs=require('fs');
 
-// fs.writeFileSync('op.txt','My name is prabesh bista.')
+fs.writeFileSync('op.txt','My name is prabesh bista,Have a good time!')
 readline=fs.readFileSync('op.txt').toString();
 console.log(readline);
