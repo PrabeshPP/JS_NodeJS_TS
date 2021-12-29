de147713f24c15cf9d88c162ecf7622222f8f85d
Prabesh <prabesh.bista369@gmail.com>
2021-12-29 17:58:23 +0545

    basic challenge

---
+++ gettingstarted/challenge.js
@@ -0,0 +1,15 @@
+let x=10;
+let name1="Prabehs";
+console.log(x+name1);
+// the above line of code would concatenate the integer and string
+if(typeof(name1)=="string"){
+    console.log("Yes,it is String");
+}else{
+    console.log("it is not a string");
+}
+
+//concat two string
+
+let name2="Prabesh";
+let lastname='Bista';
+console.log(name2+lastname);
