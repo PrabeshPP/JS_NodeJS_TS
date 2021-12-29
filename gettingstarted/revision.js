3baebbe58c1744f155703f73f6126170aa4d8f62
Prabesh <prabesh.bista369@gmail.com>
2021-12-29 18:41:41 +0545

    comarison operator

--- gettingstarted/revision.js
+++ gettingstarted/revision.js
@@ -1,6 +1,15 @@
-//
+// prefix increment ,post increment
 let x=0;
-
 console.log(++x); //prefix increment
 console.log(x++); //postfix increment
+
+//comparison operators
+
+let num1=22;
+let num3=44;
+if(num1>=num3){
+    console.log("Number 1 is greater than number 2");
+}else{
+    console.log("Number 2 is greater than number 1");
+}
 
