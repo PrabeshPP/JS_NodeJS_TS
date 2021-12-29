39c79dc7829ea4d9dcb4510ed79f47d9a934af67
Prabesh <prabesh.bista369@gmail.com>
2021-12-29 19:06:34 +0545

    operator

--- gettingstarted/revision.js
+++ gettingstarted/revision.js
@@ -44,8 +44,27 @@ console.log("Yo,I am OR Operator");
 console.log(num1<num2 ||nu1>=num2); //it should return true,since one expression it true
 
 //Logical NOT Operator
-// it willn return oposite of the expression
+// it will return oposite of the expression result
 console.log("Yo,I am a not operator");
 console.log(!(num1<num2 ||nu1>=num2));
+
+//String Operator
+
+let sentence="Hello,My name is prabesh bista.I am tikapur kailali.";
+// this will split the string wheneve it encounters space.
+console.log(sentence.split(' '));
+// using the + symbol would concatenate the two string
+console.log(sentence+"This is added String");
+//It check whether the first word is same as the one we gave
+console.log(sentence.startsWith("Hello","Hello"));
+
+
+//Output of 2**2
+//** operator returns the square of 2
+console.log(2**2);
+
+
+
+
 
 
