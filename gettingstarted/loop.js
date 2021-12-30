5e0627d4b70eac87809a15563d3d2e9331939eae
Prabesh <prabesh.bista369@gmail.com>
2021-12-30 14:15:48 +0545

    pop,shift,unShift,push,For In,For of

--- gettingstarted/loop.js
+++ gettingstarted/loop.js
@@ -16,3 +16,37 @@ for(let x in arr){
 for(let x of arr){
     console.log(x);
 }
+
+//For each Loop
+arr.forEach((elements,index,array)=>console.log(elements));
+
+//searching and filter
+console.log(arr.indexOf('r',1));
+
+//CURD in Array
+
+
+//push method
+// addd the element at the last of an array
+arr.push('hello');
+console.log(arr);
+
+
+//unShift method
+//adds the element at the first of an array
+arr.unshift("prabesh");
+console.log(arr);
+
+
+//Removing the element
+//PoP method returns the last element of an array
+arr.pop();
+console.log(arr);
+
+//shift() method
+//removes the last element of ann array
+arr.shift();
+console.log(arr);
+
+
+
