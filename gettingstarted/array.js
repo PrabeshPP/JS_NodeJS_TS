3538723385ac1d3f40c128460cd18a87b5e593ab
Prabesh <prabesh.bista369@gmail.com>
2021-12-30 13:21:07 +0545

    slice,splice,sort

--- gettingstarted/array.js
+++ gettingstarted/array.js
@@ -8,3 +8,12 @@ selectionColors[2]='gray';
 console.log(selectionColors);
 selectionColors[3]=100;
 console.log(selectionColors);
+
+let arrayIndex=[99,1,7,37,34,60];
+//sorting the array
+console.log(arrayIndex.sort());
+//Slicing the array
+console.log(arrayIndex.slice(1,4));
+//Splice delete the for a given index from to;
+console.log(arrayIndex.splice(1,3));
+console.log(arrayIndex);
