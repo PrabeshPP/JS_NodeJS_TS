4cce54f885df950771e00b189054395b56f280bb
Prabesh <prabesh.bista369@gmail.com>
2021-12-30 13:38:07 +0545

    For,For in,For of

--- gettingstarted/loop.js
+++ gettingstarted/loop.js
@@ -1,8 +1,18 @@
-let arr=["p","r","a","b","e","s","h"]
+let arr=["p","r","a","b","e","s","h"];
 
-//For Loop
+// For Loop
 for(var i=0;i<arr.length;i++){
     console.log(arr[i]);
 }
 
 //For in Loop
+//it returns the index if an array
+for(let x in arr){
+    console.log(x);
+}
+
+//For of Loop
+//it returns the elemnts of an array
+for(let x of arr){
+    console.log(x);
+}
