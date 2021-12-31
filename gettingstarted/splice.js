d79c44f8d751344470240bfb7746a236468d5ad7
Prabesh <prabesh.bista369@gmail.com>
2021-12-31 20:28:38 +0545

    splice,add,update,delete

--- gettingstarted/splice.js
+++ gettingstarted/splice.js
@@ -4,3 +4,9 @@ let arr=["jan","feb","march"];
 arr.splice(3,0,"april");
 arr.splice(4,0,"may");
 console.log(arr);
+
+//updating an array using splice method
+
+const updateMonth=arr.splice(1,1,"Feb");
+console.log(arr);
+console.log(updateMonth);
