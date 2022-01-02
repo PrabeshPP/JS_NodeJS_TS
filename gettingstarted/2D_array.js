026c8ef5de3ac4b9dc412bdf90fb0f8c346e1698
Prabesh <prabesh.bista369@gmail.com>
2022-01-02 20:46:06 +0545

    Reduce ,flattening the array

---
+++ gettingstarted/2D_array.js
@@ -0,0 +1,10 @@
+const arr=[
+    ['Zone_1','Zone_2'],
+    ['Zone_3','Zone_4'],
+    ['Zone_5','Zone_6'],
+    ['Zone_7','Zone_8']
+]
+//flattening the array
+let flat_arr=arr.reduce((accumulator,currenElem)=>{
+    return  accumulator.concat()
+})
