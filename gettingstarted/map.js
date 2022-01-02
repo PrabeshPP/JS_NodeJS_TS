1505064bc4fed55445cf2853c33f5a7b075ced36
Prabesh <prabesh.bista369@gmail.com>
2022-01-02 16:01:05 +0545

    map,chainMethod,filter,reduce

--- gettingstarted/map.js
+++ gettingstarted/map.js
@@ -1,7 +1,20 @@
-let arr=[25,36,49,64,81];
-let newArr=arr.map((currentElem,index,arr)=>{
-   return Math.sqrt(currentElem);
+// let arr=[25,36,49,64,81];
+// let newArr=arr.map((currentElem,index,arr)=>{
+//    return Math.sqrt(currentElem);
 
+// });
+
+// console.log(newArr);
+
+//Chain Method in Map
+
+let arr1=[2,3,4,6,8];
+
+let newArr1=arr1.map((currentElem,index,arr)=>{
+    return currentElem*2
+
+}).filter((currentElem)=>{
+    return currentElem>10;
 });
 
-console.log(newArr);
+console.log(newArr1)
