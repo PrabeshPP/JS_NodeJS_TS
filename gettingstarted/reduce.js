dfa9a13c46ee6ec40ac17d1cf256cdc7d7c21b32
Prabesh <prabesh.bista369@gmail.com>
2022-01-02 20:33:32 +0545

    reduce

--- gettingstarted/reduce.js
+++ gettingstarted/reduce.js
@@ -1,10 +1,18 @@
 //We use reduce method to flatten the array,eg. from 3 dimensional array yo 2 dimensional
-let arr3=[1,2,3,4,4,5,6];
+let arr3=[1,2,3,7,4,5,6];
 // Four arguments are required
 
 //Accumulator
 //Current Value
 //Current Index
 //Source Array
+let sum=arr3.map((currentElem)=>currentElem*2)
+.filter((currentElem)=>currentElem>10)
+.reduce((accumulator,currentElem1)=>
+{ 
+    return accumulator+=currentElem1;
+}
+);
 
+console.log(sum);
 
