15f2e4932f5268f600947e1f572fc386b4f8e534
Prabesh <prabesh.bista369@gmail.com>
2022-01-03 10:25:34 +0545

    string

---
+++ gettingstarted/strings.js
@@ -0,0 +1,26 @@
+// Strings Method.
+// We can use double or single characters.
+// Strings can be created as primitives.
+
+//new line Character.
+// '\n' is a is new line character.
+
+console.log("My Name is prabeshh bista. \nI am from Tikapur,kailali ");
+
+// Strings Constructors
+
+let myName=new String("Prabesh Bista");
+
+console.log(myName);
+
+//Finding the length
+
+console.log(myName.length);
+
+// Escape Character
+
+console.log("I am a web and \"Flutter\" Mobile applications developer.")
+
+
+
+
