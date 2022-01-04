4d6fd18084531aafdad9953de6299bb7797b0748
Prabesh <prabesh.bista369@gmail.com>
2022-01-04 19:00:24 +0545

    date

---
+++ gettingstarted/date.js
@@ -0,0 +1,13 @@
+// // // There are 4 ways to create new date objects
+// let currentDate=new Date();
+// console.log(currentDate.toLocaleString());
+
+// // console.log(Date.now());
+
+// let date=new Date(2018,11,24,10,33,30,0);
+// console.log(date.toLocaleString());
+// console.log(date.getDate());
+
+//Practice time
+console.log(new Date().toLocaleTimeString());
+
