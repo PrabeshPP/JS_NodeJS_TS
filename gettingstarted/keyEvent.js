322a38fd16c0ca245f4b33471b346de460fdf201
Prabesh <prabesh.bista369@gmail.com>
2022-01-06 15:37:22 +0545

    keyboard event

---
+++ gettingstarted/keyEvent.js
@@ -0,0 +1,13 @@
+const keyPress=document.getElementById('keys');
+function keypress(event){
+    return keyPress.innerHTML=`${event.key} ${event.code}`;
+}
+
+function keyUP(){
+    return keyPress.innerHTML="Key is Up";
+
+}
+function keyDown(){
+    return keyPress.innerHTML="Key is Up";
+
+}
