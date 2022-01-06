5df8475164d1c9129c553f0d295d98bb8811599a
Prabesh <prabesh.bista369@gmail.com>
2022-01-06 16:51:00 +0545

    event

--- gettingstarted/keyEvent.js
+++ gettingstarted/keyEvent.js
@@ -1,17 +1,20 @@
 
-function selectElement(value){
+function selectElement(){
     const evnt=document.getElementById('result');
     const input=document.getElementById('field');
-    if(value=="red"){
-        evnt.innerHTML=`you choosed a ${value} colors as your favourite color`;
-        evnt.style.color='red';
-    }else if(value=='blue'){
-        evnt.innerHTML=`you choosed a ${value} colors as your favourite color`;
-        evnt.style.color='blue';
-    }else{
-        evnt.innerHTML=`you choosed a ${value} colors as your favourite color`;
-        evnt.style.color='purple';
-    }
+    const Colors=document.getElementById('colors');
+   
+
+  if(Colors.value=='red'){
+      evnt.innerHTML=`${input.value} choosed ${Colors.value}`;
+      evnt.style.color='red';
+  }else if(Colors.value=='blue'){
+    evnt.innerHTML=`${input.value} choosed ${Colors.value}`;
+    evnt.style.color='blue';
+  }else {
+    evnt.innerHTML=`${input.value} choosed ${Colors.value}`;
+    evnt.style.color='purple';
+  }
   
   
     
