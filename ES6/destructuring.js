a2c9f30b322ae0435ac641d13a51fa7688b534a5
Prabesh <prabesh.bista369@gmail.com>
2022-01-07 12:36:24 +0545

    array and object destructuring

--- ES6/destructuring.js
+++ ES6/destructuring.js
@@ -2,3 +2,17 @@
 const myBioData=["prabesh","bista",19];
 let [fName,lName,age,myDegree="A-Levels"]=myBioData;;
 console.log(myDegree);
+
+
+// destructuring the object
+
+const myBioData1={
+    myName1:"Prabesh Bista",
+    age1:20,
+
+}
+
+let{myName1,age1}=myBioData1;
+
+console.log(myName1);
+console.log(age1);
