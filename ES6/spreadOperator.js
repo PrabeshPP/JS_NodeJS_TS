ae0a295c9dfe494e8eefd76675f6b359f7f1b1e1
Prabesh <prabesh.bista369@gmail.com>
2022-01-07 12:49:45 +0545

    spread Operator

--- ES6/spreadOperator.js
+++ ES6/spreadOperator.js
@@ -1,2 +1,5 @@
 const Colors=['red','green','yellow','white','blue'];
-\
+const myColors=["teal","purple"];
+//... is a spread operator
+const myfavcolor=[...Colors,...myColors];
+console.log(myfavcolor);
