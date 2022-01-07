c0b1440647a851f7208191a99381b5e5d1605bdf
Prabesh <prabesh.bista369@gmail.com>
2022-01-07 13:00:36 +0545

    array includes,**

--- ES6/arrayIncludes.js
+++ ES6/arrayIncludes.js
@@ -1,3 +1,7 @@
 // array include
 const Colors=['red','green','yellow','white','blue'];
 const isPresent=Colors.includes('yellow');
+console.log(isPresent);
+
+// ** exponential operator
+console.log(2**2);
