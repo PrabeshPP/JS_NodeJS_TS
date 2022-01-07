fbd23e6639571dbaf0b54857f4f30201f7d9fc8d
Prabesh <prabesh.bista369@gmail.com>
2022-01-07 11:54:55 +0545

    object-->

---
+++ OOPS/object.js
@@ -0,0 +1,10 @@
+let bioData={
+    myname:"Prabesh Bista",
+    myAge:26,
+    getData:function(){
+        console.log(`My name is ${bioData.myname},and my age is ${bioData.myAge}`);
+    }
+
+}
+
+bioData.getData();
