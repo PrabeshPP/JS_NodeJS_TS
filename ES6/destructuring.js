000f349c69dd7ba6225be1a5e4026202800304ec
Prabesh <prabesh.bista369@gmail.com>
2022-01-07 12:31:51 +0545

    array destructuing

--- ES6/destructuring.js
+++ ES6/destructuring.js
@@ -1,2 +1,4 @@
-const myBioData=["tikapur","kathmandu","lalitpur","bhaktapur"];
-console.log(myBioData);
+//Array destructure
+const myBioData=["prabesh","bista",19];
+let [fName,lName,age,myDegree="A-Levels"]=myBioData;;
+console.log(myDegree);
