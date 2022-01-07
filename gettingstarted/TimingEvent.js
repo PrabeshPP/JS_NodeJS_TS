3acd902ad3026236c36b23f74ce7cbbde92eb793
Prabesh <prabesh.bista369@gmail.com>
2022-01-07 09:26:41 +0545

    timingEvent

---
+++ gettingstarted/TimingEvent.js
@@ -0,0 +1,5 @@
+//The window object allows execution of code at specified time intervals
+//These time intervals are called timing events
+//There are two keys methods to use with JavaScript are:
+//setTimeOut();
+//setInterval();
