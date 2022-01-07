3c7660ca81e4554124a7f99af518fa699c4d0a9e
Prabesh <prabesh.bista369@gmail.com>
2022-01-07 15:59:59 +0545

    object.values,keys,enteries()

--- ES8/ES8.js
+++ ES8/ES8.js
@@ -1,3 +1,20 @@
 //String padding
+
+let message="Hello,My name is Prabesh Bista";
+console.log(message);
+console.log(message.padStart(message.length+8));
+message.padEnd(message.length+10);
+
 // Object.value()
 //Object.entries()
+
+const person={
+    name:"Prabesh Bista",
+    age:200
+};
+
+// It gives the values of the object.
+console.log(Object.values(person));
+
+//It gives the key of the object
+console.log(Object.entries(person));
