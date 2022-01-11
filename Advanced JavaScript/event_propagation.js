35bcf062cd10eb4ba2d197d2858d94f140482409
Prabesh <prabesh.bista369@gmail.com>
2022-01-11 21:11:58 +0545

    html,css,evntPropagation

--- Advanced JavaScript/event_propagation.js
+++ Advanced JavaScript/event_propagation.js
@@ -1,1 +1,12 @@
 //Event propagation mode determines in which order the elements receive the event.
+ let parentDiv=document.getElementsByClassName("parent");
+
+ parentDiv[0].addEventListener('click',()=>{
+     alert("You clicked Parent Widget!");
+ })
+ 
+ let childDiv=document.getElementsByClassName('child');
+
+ childDiv[0].addEventListener('click',()=>{
+     alert("You clicked Child Widget!");
+ })
