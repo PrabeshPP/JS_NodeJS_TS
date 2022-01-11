90810e4f0ed8b9f18a315a0978ee32d3b8f5ac0d
Prabesh <prabesh.bista369@gmail.com>
2022-01-11 21:56:47 +0545

    Higher-Order,Call-back function()

--- Advanced JavaScript/OrderFuction/higher_oder.js
+++ Advanced JavaScript/OrderFuction/higher_oder.js
@@ -7,3 +7,19 @@ function add(a,b){
 function sub(a,b){
     return a-b;
 }
+
+function mult(a,b){
+    return a*b;
+}
+
+function div(a,b){
+    return b/a;
+}
+// Higher-Order Function
+function Calculator(a,b,operator){
+    return operator(a,b);
+}
+
+let finalValue=Calculator(10,2,add);
+
+console.log(finalValue);
