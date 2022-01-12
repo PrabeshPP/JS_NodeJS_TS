71c126fcaaa6fbecc7e4f6ead1fc17a9acf0c0c5
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 14:02:16 +0545

    sync

--- Advanced JavaScript/Async/sync.js
+++ Advanced JavaScript/Async/sync.js
@@ -1,16 +1,28 @@
-function func2(){
-    setTimeout(() => {
-        console.log("Calling function 2");
+// function func2(){
+//     setTimeout(() => {
+//         console.log("Calling function 2");
         
-    }, 3000);
+//     }, 3000);
     
 
+// }
+
+// function fun1(){
+//     console.log("Calling function 1");
+//     func2();
+//     console.log("Calling the function 1 again");
+// }
+
+// fun1();
+
+// function Currying
+function sum(num1){
+    return function(num2){
+        return function(num3){
+            console.log(num1,num2,num3);
+        }
+    }
+
 }
 
-function fun1(){
-    console.log("Calling function 1");
-    func2();
-    console.log("Calling the function 1 again");
-}
-
-fun1();
+sum(5)(10)(50);
