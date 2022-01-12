aa7b124535ab1f757b4429b5d7d3d09c65377d95
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 13:41:00 +0545

    basic of async await in js

--- Advanced JavaScript/Async/sync.js
+++ Advanced JavaScript/Async/sync.js
@@ -1,4 +1,16 @@
 function func2(){
-    console.log("Calling function 2");
+    setTimeout(() => {
+        console.log("Calling function 2");
+        
+    }, 3000);
+    
 
 }
+
+function fun1(){
+    console.log("Calling function 1");
+    func2();
+    console.log("Calling the function 1 again");
+}
+
+fun1();
