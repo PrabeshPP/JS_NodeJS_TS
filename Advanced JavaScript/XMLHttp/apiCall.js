2277735ba123aadaa1aff49168513005f0a4a996
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 14:24:24 +0545

    apiCall()

--- Advanced JavaScript/XMLHttp/apiCall.js
+++ Advanced JavaScript/XMLHttp/apiCall.js
@@ -1,1 +1,9 @@
-let container=document.getElementsByClassName("parent")
+let container=document.getElementsByClassName("parent");
+const request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/name/nepal")
+request.send();
+
+request.addEventListener('load',function(){
+    const [data]=JSON.parse(this.responseText);
+
+})
