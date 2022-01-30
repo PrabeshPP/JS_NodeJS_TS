1b6b9158f87bf714fefa54d34d2dca48aa60897e
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 23:09:33 +0545

    TypeAssertion in TypeScript

---
+++ TS/exp.js
@@ -0,0 +1,4 @@
+//TypeAssertion in TypeScript
+var str = '1';
+var str2 = str;
+console.log(typeof (str2));
---
+++ TS/exp.ts
@@ -0,0 +1,4 @@
+//TypeAssertion in TypeScript
+var str='1';
+var str2:number=<number><any> str;
+console.log(typeof(str2));
