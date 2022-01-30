c4aba736e6241cfb2b3612b2dc9a8355a2e106a9
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 22:44:21 +0545

    class type check

--- TS/class.ts
+++ TS/class.ts
@@ -9,6 +9,6 @@ class Person{
         this.color=color;
     }
 }
-
+//if  the objects of the class matches all the varible of the class varibale type ,it will not show any error
 const prabesh=new Person("Prabesh Bista",20,"White");
 console.log(prabesh.name);
