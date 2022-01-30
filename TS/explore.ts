c50edaefe99d899d2244ade0ee155e279c3c21ee
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 22:59:30 +0545

    exploring typescript

--- TS/explore.ts
+++ TS/explore.ts
@@ -1,1 +1,25 @@
 let helloWorld="My name is Prabesh Bista";
+
+//Definig Types
+
+const car:Vehicle={
+    model:"I20",
+    name:"Hyundai",
+    id:0,
+
+}
+
+const bike:Vehicle={
+    model:"Belini202",
+    name:"Belini",
+    id:1
+}
+
+
+
+
+interface Vehicle{
+    model:String,
+    name:String,
+    id:number
+}
