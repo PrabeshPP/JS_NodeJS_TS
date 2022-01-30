cf5e07f9610203ec120bce0b8da770ce400c726b
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 23:16:38 +0545

    abstract class in typescript and JavaScript

--- TS/class.js
+++ TS/class.js
@@ -1,3 +1,20 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
 var Person = /** @class */ (function () {
     function Person(name, age, color) {
         this.name = name;
@@ -9,10 +26,24 @@ var Person = /** @class */ (function () {
 //if  the objects of the class matches all the varible of the class varibale type ,it will not show any error
 var prabesh = new Person("Prabesh Bista", 20, "White");
 console.log(prabesh.name);
-
-
-arr=["Prabesh","Bista","Kathmandu","Pokhara"];
-for( x of arr){
-    console.log(x);
-}
-
+//abstract class
+var Base = /** @class */ (function () {
+    function Base() {
+    }
+    Base.prototype.printName = function () {
+        console.log("My Name is " + this.getName());
+    };
+    return Base;
+}());
+var child = /** @class */ (function (_super) {
+    __extends(child, _super);
+    function child() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    child.prototype.getName = function () {
+        return "Prabesh Bisra";
+    };
+    return child;
+}(Base));
+var d = new child();
+d.printName();
--- TS/class.ts
+++ TS/class.ts
@@ -14,3 +14,24 @@ class Person{
 //if  the objects of the class matches all the varible of the class varibale type ,it will not show any error
 const prabesh=new Person("Prabesh Bista",20,"White");
 console.log(prabesh.name);
+
+//abstract class
+
+abstract class Base{
+    abstract getName():String;
+
+    printName(){
+        console.log("My Name is "+this.getName());
+    }
+
+}
+
+class child extends Base{
+    getName(): String {
+        return "Prabesh Bista";
+    }
+
+}
+
+const d=new child();
+d.printName();
