9ca57021a7444c34e51fba7b886c8123e95bde3b
Prabesh <prabesh.bista369@gmail.com>
2022-03-03 19:39:14 +0545

    mind-grenade module

--- nodejs/NodeFundamentals/app.js
+++ nodejs/NodeFundamentals/app.js
@@ -4,16 +4,20 @@ const names=require('./1-module');
 const Hello=require('./2-module');
 const data=require('./3-alternative');
 
+//!Even without exporting the module,we will be able to call the addValues() function
+
+require('./7-mind-grenade');
 
 
-Hello(names.Prabesh);
-Hello(names.Carlo);
 
-let items=data.items;
+// Hello(names.Prabesh);
+// Hello(names.Carlo);
 
-for(const item of items){
-    console.log(item);
-}
+// let items=data.items;
+
+// for(const item of items){
+//     console.log(item);
+// }
 
 
-console.log(data.singlePerson.name);
+// console.log(data.singlePerson.name);
