5eb334fb6130d1fd883ef3487ce44789e8344beb
Prabesh <prabesh.bista369@gmail.com>
2022-03-03 19:31:37 +0545

    Alternative flavour in JavaScript

--- nodejs/NodeFundamentals/app.js
+++ nodejs/NodeFundamentals/app.js
@@ -2,6 +2,18 @@
 
 const names=require('./1-module');
 const Hello=require('./2-module');
+const data=require('./3-alternative');
+
+
 
 Hello(names.Prabesh);
 Hello(names.Carlo);
+
+let items=data.items;
+
+for(const item of items){
+    console.log(item);
+}
+
+
+console.log(data.singlePerson.name);
