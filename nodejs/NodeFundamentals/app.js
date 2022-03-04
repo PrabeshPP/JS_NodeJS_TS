29a0e8498461ecfe77042f618f59020518709324
Prabesh <prabesh.bista369@gmail.com>
2022-03-04 09:19:17 +0545

    Os Built in Modules

--- nodejs/NodeFundamentals/app.js
+++ nodejs/NodeFundamentals/app.js
@@ -5,3 +5,19 @@ console.log(os.userInfo().username);
 //methods returns the system uptime in seconds
 
 console.log(`The system is ${os.uptime()} seconds`);
+
+
+//
+
+const currentOS={
+    name:os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+    freeMem:os.freemem(),
+
+}
+
+
+console.log(currentOS);
+
+
