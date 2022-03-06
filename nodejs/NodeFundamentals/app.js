8afbaa0527e8e0e0d582f75043becc454cbb4407
Prabesh <prabesh.bista369@gmail.com>
2022-03-06 16:00:52 +0545

    fs readFile async

--- nodejs/NodeFundamentals/app.js
+++ nodejs/NodeFundamentals/app.js
@@ -4,7 +4,7 @@ const path=require('path');
 //
 const file_path=path.resolve(__dirname,'Dymmy','dymmy1.1','dummy.txt');
 
-const data=fs.readFile(file_path,'utf-8',(err,result)=>{
+fs.readFile(file_path,'utf-8',(err,result)=>{
     if(err){
         return "There is no such file";
     }else{
@@ -13,4 +13,12 @@ const data=fs.readFile(file_path,'utf-8',(err,result)=>{
     }
 },)
 
-// console.log(data);
+fs.writeFile(file_path,'\nMy name is Prabesh Bista.',{flag:'a'},(err,result)=>{
+    if(err){
+        console.log("There is an error in writing to the file!");
+        return ;
+    }
+   
+})
+
+
