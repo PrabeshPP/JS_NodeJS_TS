b899fc5f97f6f8440593b2a837e620016d953c01
Prabesh <prabesh.bista369@gmail.com>
2022-03-06 15:45:51 +0545

    read file in an async

--- nodejs/NodeFundamentals/app.js
+++ nodejs/NodeFundamentals/app.js
@@ -1,1 +1,16 @@
+const fs=require('fs');
+const path=require('path');
 
+//
+const file_path=path.resolve(__dirname,'Dymmy','dymmy1.1','dummy.txt');
+
+const data=fs.readFile(file_path,'utf-8',(err,result)=>{
+    if(err){
+        return "There is no such file";
+    }else{
+        console.log(result);
+        return;
+    }
+},)
+
+// console.log(data);
