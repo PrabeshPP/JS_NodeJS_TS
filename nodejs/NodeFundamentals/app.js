497bc6b365fa009f5960776dcd9ab3a0cbabe9f3
Prabesh <prabesh.bista369@gmail.com>
2022-03-09 17:41:27 +0545

    Streams in NodeJS

--- nodejs/NodeFundamentals/app.js
+++ nodejs/NodeFundamentals/app.js
@@ -1,2 +1,7 @@
 const fs=require('fs');
+const path=require('path');
 
+const filePath=path.resolve(__dirname,'Dymmy','dymmy1.1','big.txt');
+for(let i=0;i<1000;i++){
+    fs.writeFileSync(filePath,`Hello ${i}\n`,{flag:'a'})
+}
