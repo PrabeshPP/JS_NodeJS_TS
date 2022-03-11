8c9ea83fed485376ba247168d2d380f404336ef9
Prabesh <prabesh.bista369@gmail.com>
2022-03-11 21:31:30 +0545

    Learned about Routing

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -3,3 +3,11 @@ const app=express();
 app.get("/",(req,res)=>{
   res.end("You are in the home Page!")
 })
+
+app.get("/about",(req,res)=>{
+  res.end("You are in the about Page.")
+})
+
+app.listen(3000,()=>{
+  console.log("listening at port 3000");
+})
